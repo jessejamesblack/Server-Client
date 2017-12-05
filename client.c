9749fa570b9cc6a629fc9b2ed567b345f229c7f1
Brian Schillaci <schillaci1579@gmail.com>
2017-12-04 22:52:51 -0500

    got it to send a file from client to server, still a lot to do, but on the right path

--- client.c
+++ client.c
@@ -1,12 +1,14 @@
 #include "socket.h"
+#include <stdlib.h>
 #define PORT 8080
+#define TEST_SIZE 7
 
 int main(int argc, char const *argv[])
 {
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    char *hello = "1024";
     char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -20,7 +22,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, "128.6.13.188", &serv_addr.sin_addr) <= 0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
@@ -31,9 +33,19 @@ int main(int argc, char const *argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
+
+    // Need to add code to receive connection id that will later be used to request sorted file and close connection with server.mak
+    // Need to change to create thread for each csv file and traverse directory otherwise.
+    FILE *fp = fopen("movie_metadata.csv", "r");
+
+    // Throwing away the header, server is hard coded for this .csv file.
+    fgets(buffer, 1024, fp);
+    
+    while (fgets(buffer, 1024, fp) != NULL) {
+	write(sock, buffer, strlen(buffer));
+    }
+
+    fclose(fp);
+
     return 0;
 }
